f1cb8add8625e5a3320ecc314debbe1819ca88c9
Robert McFarlane <rob.mcfarlane@hotmail.co.uk>
2016-11-15 12:38:36 +0000

    added basic functions
    added:
    -addBox() which checks boxes are valid before adding them
    -totalCost()
    -numBoxes()
    -numBoxTypes()
    
    as of now all seem to work. Some may be removed later if deemed unnecessary

--- Order.java
+++ Order.java
@@ -1,14 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package adproc.coursework;
 
+import java.util.ArrayList;
+
 /**
- *
  * @author up790070
  */
 public class Order {
+    ArrayList<Box> boxes = new ArrayList();
+
+    public void addBox(Box toAdd) {
+        if (toAdd.checkBox()) {
+            boxes.add(toAdd);
+        } else {
+            System.out.println("Invalid box: Box not added to Order!");
+        }
+    }
+
+    public double totalCost() {
+        double cost = 0;
+        for (int i = 0; i < boxes.size(); i++) {
+            cost += boxes.get(i).calcCost();
+        }
+        return cost;
+    }
     
+    public int numBoxes(){
+        int amount = 0;
+        for(int i=0; i<boxes.size(); i++){
+            amount += boxes.get(i).quantity;
+        }
+        return amount;
+    }
+    
+    public int numBoxTypes(){
+        return boxes.size();
+    }
 }
