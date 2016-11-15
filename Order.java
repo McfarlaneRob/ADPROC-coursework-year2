33123b1e9d0843daefa73a6b9e062c9810f6e3b9
Robert McFarlane <rob.mcfarlane@hotmail.co.uk>
2016-11-15 16:05:19 +0000

    added function description comments
    the functions all now have a comment above them to describe what they do for ease of reading when they are collapsed in the editor

--- Order.java
+++ Order.java
@@ -10,6 +10,7 @@ public class Order {
 
     ArrayList<Box> boxes = new ArrayList();
 
+    // adds valid boxes to end of list, otherwise rejects wth message
     public void addBox(Box toAdd) {
         if (toAdd.checkBox()) {
             boxes.add(toAdd);
@@ -18,6 +19,7 @@ public class Order {
         }
     }
 
+    // outputs the total cost of all boxes in the order
     public double totalCost() {
         double cost = 0;
         for (int i = 0; i < boxes.size(); i++) {
@@ -26,6 +28,7 @@ public class Order {
         return cost;
     }
 
+    // ouutputs number of boxes in the current order
     public int numBoxes() {
         int amount = 0;
         for (int i = 0; i < boxes.size(); i++) {
@@ -34,10 +37,12 @@ public class Order {
         return amount;
     }
 
+    // outputs the number of items in the list
     public int numBoxTypes() {
         return boxes.size();
     }
 
+    // removes all box objects from the list
     public void clearOrder() {
         while(boxes.size() > 0){
             boxes.remove(0);
