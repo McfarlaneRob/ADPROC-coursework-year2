e4120b9a66ef0698f521b0dbe392cf5e09c465ee
Robert McFarlane <rob.mcfarlane@hotmail.co.uk>
2016-11-29 13:21:16 +0000

    Create Box_2.java

---
+++ Box_2.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package adproc.coursework;
+
+/**
+ *
+ * @author up790070
+ */
+public class Box_2 extends Box {
+
+    public Box_2(int w, int h, int l, int grade, Boolean top, int num) {
+        width = w;
+        height = h;
+        length = l;
+        cardGrade = grade;
+        sealableTop = top;
+        quantity = num;
+        cornerReinforce = false;
+        bottomReinforce = false;
+        colours = 1;
+
+    }
+
+}
