4fa21909fb1de33fe8ccf6a7479e7113f66687a2
Robert McFarlane <rob.mcfarlane@hotmail.co.uk>
2016-12-03 14:14:35 +0000

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,18 +1,8 @@
 package adproc.coursework;
 
-/**
- *
- * @author up790070
- */
 public class Main {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        Order order = new Order();
         GUI gui = new GUI();
         gui.setVisible(true);
     }
-
 }
